deb88927599c844cd5a51ccec3057fe04bd0181f
Lucas <lucasoliveirars22@gmail.com>
2024-08-02 13:41:44 -0300

    fix origin cors

--- server/index.js
+++ server/index.js
@@ -11,14 +11,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
     methods: ["GET", "POST"],
   },
 });
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
     methods: ["GET", "POST"],
   })
 );
